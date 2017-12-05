0aa42195823ea2d6d160ed11f96b43346c109d51
bgaesop <bgaesop@gmail.com>
2017-12-04 17:32:41 -0800

    add color and size changing dancers who move

--- src/makeColorDancer.js
+++ src/makeColorDancer.js
@@ -11,21 +11,6 @@ makeColorDancer.prototype = new makeSlidingDancer;
 makeColorDancer.prototype.step = function() {
   makeSlidingDancer.prototype.step.call(this);
   var whereTo = Math.random();
-  if (whereTo < 0.25) {
-//    this.$node
-//    this.$node.animate({'color': 'green'});
-//    this.$node.animate({ 'marginLeft' : 500}, 500);
-//    this.$node.animate({height:'100'});
-  } else if (whereTo < 0.5) {
-//    this.$node.animate({ 'marginLeft' : 100}, 500);
-//    this.$node.animate({height:'10'});
-  } else if (whereTo < 0.75) {
-//    this.$node.animate({ 'marginTop' : 0}, 500);
-//    this.$node.animate({width:'100'});
-  } else {
-//    this.$node.animate({'marginTop' : 100}, 500);
- //   this.$node.animate({width: ''})
-  }
 };
 
 makeColorDancer.prototype.constructor = makeColorDancer;
